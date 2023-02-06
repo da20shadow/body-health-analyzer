312b9b1a92296759e0f9e00d308d645092880d37
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-06 12:23:03 +0200

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SleepController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/** ------------------------Public------------------------ */
+Route::post('login',[AuthController::class,'login']);
+Route::post('register',[AuthController::class,'register']);
+
+/** ----------------------Protected----------------------- */
+Route::group(['middleware' => ['auth:sanctum']],function () {
+
+    /** ----------USER---------- */
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    /** ----------SLEEP---------- */
+    Route::post('sleep', [SleepController::class, 'index']);
+    Route::post('sleep/add', [SleepController::class, 'index']);
+
 });
